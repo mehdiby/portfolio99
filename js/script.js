f1d11e05fb90a1d651ecab4ef7dac416ce3aaf6f
mehdiby <mehdi.benyarou@esprit.tn>
2024-01-04 12:17:14 +0100

    skills

--- js/script.js
+++ js/script.js
@@ -89,3 +89,24 @@ document.getElementById('Shazam').addEventListener('click', function() {
 });
 
 
+
+//  Toolkit rotating logos animation
+
+const logosWrappers = document.querySelectorAll(".logo-group");
+
+const sleep = (number) => new Promise((res) => setTimeout(res, number));
+
+logosWrappers.forEach(async (logoWrapper, i) => {
+  const logos = Array.from(logoWrapper.children);
+  await sleep(1400 * i);
+  setInterval(() => {
+    let temp = logos[0];
+    logos[0] = logos[1];
+    logos[1] = logos[2];
+    logos[2] = temp;
+    logos[0].classList.add("hide", "to-top");
+    logos[1].classList.remove("hide", "to-top", "to-bottom");
+    logos[2].classList.add("hide", "to-bottom");
+  }, 5600);
+});
+
