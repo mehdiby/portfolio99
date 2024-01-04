bc44725eb0d7e314a1754f8a9deaa2874cac8ad5
mehdiby <mehdi.benyarou@esprit.tn>
2024-01-04 16:54:44 +0100

    testimonials

--- js/script.js
+++ js/script.js
@@ -110,3 +110,31 @@ logosWrappers.forEach(async (logoWrapper, i) => {
   }, 5600);
 });
 
+
+// Testimonial carousel
+
+jQuery(document).ready(function($) {
+  "use strict";
+  //  TESTIMONIALS CAROUSEL HOOK
+  $('#customers-testimonials').owlCarousel({
+      loop: true,
+      center: true,
+      items: 3,
+      margin: 0,
+      autoplay: true,
+      dots:true,
+      autoplayTimeout: 8500,
+      smartSpeed: 450,
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 2
+        },
+        1170: {
+          items: 3
+        }
+      }
+  });
+});
